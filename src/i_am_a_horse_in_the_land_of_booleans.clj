b10fc40ba6fa9528031a22f0bd8e1b297dd432a8
Code <bru8927@CORPBRU8927A.hca.corpad.net>
2014-02-21 15:55:22 -0600

    finished + tested

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -5,24 +5,35 @@
 
 
 (defn abs [x]
-  ":(")
+  (if (neg? x) (- x) (+ x)))
 
-(defn divides? [divisor n]
-  ":(")
+
+(defn divides? [divisor n] (if (= (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+    (cond
+     (= 0 (mod n 15)) "gotcha!"
+     (= 0 (mod n 3)) "fizz"
+     (= 0 (mod n 5)) "buzz"
+     :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen? [age] (>= 19 (int age) 13))
 
 (defn not-teen? [age]
-  ":(")
+(not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (= (count x) 0) nil
+   (or (list? x) (vector? x) ) (* 2 (count x))
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (= 0 (mod year 400)) true
+    (= 0 (mod year 100)) false
+    (= 0 (mod year 4)) true
+    :else false))
 
 ; '_______'
